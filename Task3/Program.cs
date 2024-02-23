ed2db9c62d020f36dcab058d6cad6bcca7ae665a
Azat Zuppar <zuppar.azat@gmail.com>
2024-02-23 15:57:52 +0600

    add Lesson2_Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 10;
+int[] array = {2, 5, 4, 7, 8, 35, 1, 3, 8, 5};
+int i = 0;
+while(i < n)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
